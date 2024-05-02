097001dabe52ba9a22a7228c1f4c7c08699b38dd
Mustapha tahiru <tahirumustapha13@gmail.com>
2024-05-02 23:49:46 +0000

    added contact  component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -12,30 +12,33 @@ const Contact = () => {
           </h4> 
         </div>
       </div>
-      <div className=' w-full md:w-[80%]  mx-auto  md:flex items-center'>
-        <div className=' w-full '>
+      <div className=' w-full md:w-[80%] flex-col  mx-auto  md:flex md:flex-row items-center'>
+        <div className=' w-full mt-10 '>
           <form  className=''>
-            <div className=' flex w-full items-center gap-4 mb-8'>
-              <input type="text" placeholder='Your Name' className=' w-[45%] outline-none px-3 py-3 rounded-md border-gray-200 border-[0.3px]' />
-              <input type="email" placeholder='Your Email' className=' w-[45%] outline-none px-3 py-3 rounded-md border-gray-200 border-[0.3px]' />
+            <div className=' flex-col  flex px-2  md:flex-row w-full items-center gap-4 mb-4 md:gap-4 md:mb-8'>
+              <input type="text" placeholder='Your Name' className=' w-full md:w-[45%] outline-none px-3 py-3 rounded-md border-gray-200 border-[0.3px] ' />
+              <input type="email" placeholder='Your Email' className=' w-full md:w-[45%] outline-none px-3 py-3 rounded-md border-gray-200 border-[0.3px]' />
             </div>
-            <div className=' flex w-full items-center gap-4 mb-8'>
-              <input type="number" placeholder='Your Phone' className='w-[45%] outline-none px-3 py-3 rounded-md border-gray-200 border-[0.3px]' />
-              <input type="text" placeholder='Your Location' className='w-[45%] outline-none px-3 py-3 rounded-md border-gray-200 border-[0.3px]' />
+            <div className=' flex flex-col px-2 md:flex-row  w-full items-center gap-4 mb-4 md:gap-4 md:mb-8'>
+              <input type="number" placeholder='Your Phone' className=' w-full md:w-[45%] outline-none px-3 py-3 rounded-md border-gray-200 border-[0.3px]' />
+              <input type="text" placeholder='Your Location' className=' w-full  md:w-[45%] outline-none px-3 py-3 rounded-md border-gray-200 border-[0.3px]' />
               </div>
               
            
-            <div className=' flex w-full items-center gap-4 mb-8'>
-              <select className='w-[45%] outline-none text-gray-400 px-3 py-4    rounded-md  '>
+            <div className=' flex flex-col px-2 md:flex-row w-full items-center gap-4 mb-4  md:gap-4 md:mb-8'>
+              <select className=' w-full md:w-[45%] outline-none text-gray-400 px-3 py-4    rounded-md  '>
                 <option className=' ' >Your Service</option>
                 <option >Home Cleaning</option>
                 <option >Office Cleaning</option>
                 <option >Kitchen Cleaning</option>
               </select>
-              <input type="text" placeholder='Your Date' className='w-[45%] px-3 py-3 rounded-md outline-none border-gray-200 border-[0.3px]' />
+              <input type="text" placeholder='Your Date' className=' w-full md:w-[45%] px-3 py-3 rounded-md outline-none border-gray-200 border-[0.3px]' />
             </div>
-            <div>
-              <textarea  placeholder='Your Massage...' className=' h-28 rounded-md  px-3 py-3  w-[92%] placeholder:text-gray-400 outline-none  border-[0.3px] text-lg'></textarea>
+            <div className=' w-full px-2'>
+              <textarea  placeholder='Your Massage...' className=' h-28 rounded-md w-full  px-3 py-3  md:w-[92%] placeholder:text-gray-400 outline-none  border-[0.3px] text-lg'></textarea>
+            </div>
+            <div className=' w-full  ml-2 mt-6'>
+              <button className=' bg-blue-800 text-white px-6 py-3 border-yellow-300 border-[4px] hover:bg-yellow-300 hover:text-blue-800 duration-300'>Send Message</button>
             </div>
           </form>
         </div>
